a0144cb42099ceaff6f44e38da5c1f32f0aff3af
sanjay <sanjay@LAPTOP-RQE1CNIF>
2024-01-15 00:24:46 +0530

    1st commit

---
+++ src/vinaapp/TimerListener.java
@@ -0,0 +1,14 @@
+package vinaapp;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TimerListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
